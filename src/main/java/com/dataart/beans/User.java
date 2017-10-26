7cef8a31053d5f51e95b9b76ded5cad7b26be4a2
Roman <roman.avto.pashhenko@mail.ru>
2017-10-26 13:23:17 +0300

    Add bean User only for testing git work.

---
+++ src/main/java/com/dataart/beans/User.java
@@ -0,0 +1,12 @@
+package com.dataart.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by Роман on 26.10.2017.
+ */
+@Entity
+@Table(name = "users")
+public class User {
+}
